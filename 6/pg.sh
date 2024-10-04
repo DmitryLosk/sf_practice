f032183dada54d2e0479ada105860487b7170c18
DmitryLosk <dloskut@gmail.com>
2024-10-04 13:53:06 +0300

    added project files for project 10

---
+++ 6/pg.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install libpq-dev
+sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y postgresql-server-dev-all
+pip3 install psycopg2-binary Django
